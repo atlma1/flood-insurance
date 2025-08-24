9da00a4a86f68ba7d2c512c553a6cc11e455ab43
Cassie Ma <cassiema@example.com>
2025-08-24 16:19:12 -0400

    fix: updated text color for property name in PropertySelect component

--- components/flood/propertyType/propertySelect.tsx
+++ components/flood/propertyType/propertySelect.tsx
@@ -24,7 +24,7 @@ const PropertySelect: FC<PropertySelectProps> = ({ name, icon }) => {
       className="bg-background flex items-center justify-center flex-col px-[61px] pb-[29px] pt-[76px] rounded-[19px]"
     >
       <Image src={icon} alt={name} width={74} height={74} />
-      <p className="mt-[46px] font-inter font-normal text-[30px]">{name}</p>
+      <p className="mt-[46px] font-inter font-normal text-[30px] text-white">{name}</p>
     </button>
   );
 };
